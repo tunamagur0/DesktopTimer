51da34f6a0591c0b46d649e8ad69636ffd9639a4
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-05-01 17:17:51 +0900

    プロジェクト ファイルを追加します。

---
+++ DesktopTimer/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace DesktopTimer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// 時刻表示用タイマー
+        /// </summary>
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = CreateTimer();
+            timer.Start();
+
+            MoveWindowToRightTop();
+        }
+
+        private void MoveWindowToRightTop()
+        {
+            Console.WriteLine(this.Left);
+            this.Left = SystemParameters.PrimaryScreenWidth - this.Width*3;
+            this.Top = 0;
+        }
+
+
+        /// <summary>
+        /// タイマー生成
+        /// </summary>
+        /// <returns>生成したタイマー</returns>
+        private DispatcherTimer CreateTimer()
+        {
+            var t = new DispatcherTimer(DispatcherPriority.Normal);
+
+            t.Interval = TimeSpan.FromMilliseconds(200);
+
+            t.Tick += (sender, e) =>
+            {
+                textBlock.Text = DateTime.Now.ToString("HH:mm");
+            };
+
+            return t;
+        }
+
+        private void Viewbox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
